5a7f1955b3e00ce7b9e0c11327c5d37e0125d6f8
alviseaspesi <alviseaspesi@gmail.com>
2019-12-11 12:14:40 +0100

    update

--- sketch.js
+++ sketch.js
@@ -1,11 +1,21 @@
-function preload(){
-  // put preload code here
-}
-
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  angleMode(DEGREES);
 }
 
 function draw() {
-  // put drawing code here
+  background(0);
+  var posX = mouseX;
+  var posY = mouseY;
+  pointLight(125, 30, 40, -posX, -posY, 0);
+  pointLight(230, 120, 140, -posX, posY, 0);
+  pointLight(200, 80, 60, -posX, posY, 0);
+  camera(posX, posY, 50, -50, 0, 0, 0, 1, 0);
+
+  ambientMaterial(125);
+  noStroke();
+  rotateY(frameCount);
+  torus(150, 10);
+  rotateY(- 2 * frameCount);
+  sphere(100);
 }
